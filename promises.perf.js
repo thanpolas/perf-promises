27527b442eaed4b38f1fd8c8cc2b01348d0b5734
Thanasis Polychronakis <thanpolas@gmail.com>
2013-04-19 01:23:19 +0300

    latest results

--- promises.perf.js
+++ promises.perf.js
@@ -83,15 +83,15 @@ function control(runs, outputcsv) {
 
 
 var runs = [
-  [require('./packages/when2.0.1/'), 10, 'when-2.0.1']
+  // [require('./packages/when2.0.1/'), 10, 'when-2.0.1'],
   // [require('./packages/when2.0.1/'), 100, 'when-2.0.1'],
   // [require('./packages/when2.0.1/'), 500, 'when-2.0.1'],
   // [require('./packages/when2.0.1/'), 1000, 'when-2.0.1']
 
-  // [require('./packages/when1.8.1/'), 10, 'when-1.8.1'],
+  [require('./packages/when1.8.1/'), 10, 'when-1.8.1']
   // [require('./packages/when1.8.1/'), 100, 'when-1.8.1'],
   // [require('./packages/when1.8.1/'), 500, 'when-1.8.1'],
-  // [require('./packages/when1.8.1/'), 1000, 'when-1.8.1'],
+  // [require('./packages/when1.8.1/'), 1000, 'when-1.8.1']
 
   // The default when is from dev branch 2.1.x
   // [when, 10, 'when-2.1.x'],
@@ -99,18 +99,25 @@ var runs = [
   // [when, 500, 'when-2.1.x'],
   // [when, 1000, 'when-2.1.x']
 
-  // // [Q, 10, 'Q'],
-  // // [Q, 100, 'Q'],
-  // // [Q, 500, 'Q'],
-  // // [Q, 1000, 'Q']
+  // [Q, 10, 'Q'],
+  // [Q, 100, 'Q'],
+  // [Q, 500, 'Q'],
+  // [Q, 1000, 'Q']
 
   // [rsvp, 10, 'rsvp']
 
-  // [false, 10, 'async']
+  // [false, 10, 'async'],
   // [false, 100, 'async'],
   // [false, 500, 'async'],
   // [false, 1000, 'async']
 
+
+  // memory single test runs of 1k loops
+  // [require('./packages/when2.0.1/'), 1000, 'mem-when-2.0.1']
+  // [require('./packages/when1.8.1/'), 1000, 'mem-when-1.8.1']
+  // [when, 1000, 'mem-when-2.1.x']
+  // [Q, 1000, 'mem-Q']
+  // [false, 1000, 'mem-async']
 ];
 
 if (!global.gc) {
